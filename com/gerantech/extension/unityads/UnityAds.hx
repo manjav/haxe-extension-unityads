9695ce0c2faeedf4e9d0a291541fe96617f8724f
Mansour <mansurjavadi@gmail.com>
2021-01-07 10:57:23 +0330

    Add init method

--- com/gerantech/extension/unityads/UnityAds.hx
+++ com/gerantech/extension/unityads/UnityAds.hx
@@ -9,12 +9,22 @@ import lime.system.JNI;
 #end
 
 class UnityAds {
+	///callbacks haxe
+	public static var onInit:Bool->String->Void = null;
 	public static var _instance:UnityAds = null;
 
+
+	public static function init(gameId:String, testMode:Bool = false, debugMode:Bool = false):Void {
+		libInit(getInstance(), gameId, testMode, debugMode);
+	}
 	public static function getInstance():UnityAds {
 		if (_instance == null)
 			_instance = new UnityAds();
 		return _instance;
 	}
+
+
+	// event handlers
+	public function new() {}
 
 }
--- dependencies/android/src/main/java/com/gerantech/extension/unityads/UnityAdsWrapper.java
+++ dependencies/android/src/main/java/com/gerantech/extension/unityads/UnityAdsWrapper.java
@@ -1,5 +1,11 @@
 package com.gerantech.extension.unityads;
 
+import com.unity3d.ads.IUnityAdsInitializationListener;
+import com.unity3d.ads.UnityAds;
+import com.unity3d.ads.metadata.MediationMetaData;
+import com.unity3d.ads.metadata.PlayerMetaData;
+import com.unity3d.services.core.webview.WebView;
+
 import org.haxe.extension.Extension;
 import org.haxe.lime.HaxeObject;
 
@@ -14,6 +20,41 @@ public class UnityAdsWrapper extends Extension {
 	//init UnityAds with the provided gameId and set up the haxe callback. set up test mode if is specified
 	public static void init(HaxeObject cbo, String id, boolean testMode, boolean debugMode) {
 
+		if (initialized) {
+			listener.onUnityInitialize(false, "UnityAds already initialized.");
+			return;
+		}
+		callbackObject = cbo;
+		gameId = id;
+
+		listener = new UnityAdsWrapperListener();
+		UnityAds.addListener(listener);
+		UnityAds.setDebugMode(debugMode);
+        mainActivity.runOnUiThread (new Thread(new Runnable() {  
+            public void run() {
+				WebView.setWebContentsDebuggingEnabled(true);
+             }
+        }));
+
+
+		MediationMetaData mediationMetaData = new MediationMetaData(mainContext);
+		mediationMetaData.setName("mediationPartner");
+		mediationMetaData.setVersion("v12345");
+		mediationMetaData.commit();
+
+		UnityAds.initialize(mainContext, gameId, testMode, new IUnityAdsInitializationListener() {
+			@Override
+			public void onInitializationComplete() {
+				listener.onUnityInitialize(true, "initialized.");
+				initialized = true;
+			}
+
+			@Override
+			public void onInitializationFailed(UnityAds.UnityAdsInitializationError error, String message) {
+				listener.onUnityInitialize(false, message);
+			}
+		});
+	}
 	}
 
 }
